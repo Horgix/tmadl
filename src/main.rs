ee73c3445172a524b292675314c215fb5e0f964c
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2026-05-22 08:38:21 +0200

    chore(CLI): format and add comments on placeholders

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-use std::env;
 use clap::{Parser, Subcommand};
-use tabled::{Tabled, Table};
+use std::env;
+use tabled::{Table, Tabled};
 
 mod domain;
 use domain::recording::Recording;
 use domain::recording_store::RecordingStore;
 
 mod infrastructure;
-use infrastructure::s3_recording_store::S3RecordingStore;
 use infrastructure::claude_summarizer::{self, get_prompt};
+use infrastructure::s3_recording_store::S3RecordingStore;
 
 mod interface;
 use interface::ingestion;
-
 
 /// Main CLI structure
 #[derive(Parser, Debug)]
@@ -21,7 +20,7 @@ use interface::ingestion;
     version = "0.1",
     author = "Alexis Horgix Chotard <tmadl@foss.horgix.fr>",
     about = "TMA;DL (Too Much Audio, Didn't Listen) — A terminal/CLI meetings & discussions recordings processor using AI",
-    arg_required_else_help = true,
+    arg_required_else_help = true
 )]
 struct TmadlCli {
     /// Subcommands
@@ -38,7 +37,11 @@ enum TmadlSubcommands {
     /// Upload a local recording to S3
     Upload {
         /// File path of the local recording
-        #[arg(long, value_name = "FILE_PATH", help = "Path to the local recording file")]
+        #[arg(
+            long,
+            value_name = "FILE_PATH",
+            help = "Path to the local recording file"
+        )]
         path: String,
     },
 
@@ -52,13 +55,16 @@ enum TmadlSubcommands {
     /// Interrogate a transcription
     Ask {
         /// The transcription to interrogate
-        #[arg(long, value_name = "TRANSCRIPTION", help = "The transcription to interrogate")]
+        #[arg(
+            long,
+            value_name = "TRANSCRIPTION",
+            help = "The transcription to interrogate"
+        )]
         transcription: String,
-    }
+    },
 }
 
 fn main() {
-
     let cli = TmadlCli::parse();
 
     // When using upload, call ingestion parse recording etc
@@ -74,24 +80,25 @@ fn main() {
             recordings.reverse();
             let table = Table::new(recordings).to_string();
             println!("{}", table);
-
         }
         Some(TmadlSubcommands::Upload { path }) => {
             println!("Uploading recording from path: {}", path);
             let recording = ingestion::parse_recording_information_from_local_mp3_file(path);
             let s3_bucket = env::var("TMADL_S3_BUCKET_NAME").unwrap();
             let store = S3RecordingStore::new(&s3_bucket);
-            store.send_local_recording(&recording.unwrap(), path).unwrap();
+            store
+                .send_local_recording(&recording.unwrap(), path)
+                .unwrap();
         }
         Some(TmadlSubcommands::Ask { transcription }) => {
             println!("Interrogating transcription: {}", transcription);
-            let mock_recording = Recording{
-                id: "FIXME_THIS_IS_A_PLACEHOLDER".to_string(),
-                source: Some("PLACEHOLDER".to_string()),
-                date_time: Some(chrono::Utc::now()),
-                duration: Some(3600),
-                number_of_speakers: Some(2),
-                language: Some("French".to_string()),
+            let mock_recording = Recording {
+                id: transcription.clone(),
+                source: Some("PLACEHOLDER".to_string()), // FIXME
+                date_time: Some(chrono::Utc::now()),     // FIXME
+                duration: Some(3600),                    // FIXME
+                number_of_speakers: Some(2),             // FIXME
+                language: Some("French".to_string()),    // FIXME
                 description: Some("FIXME_THIS_IS_A_PLACEHOLDER".to_string()),
                 transcription: None,
             };
@@ -102,9 +109,7 @@ fn main() {
             let input = get_prompt(
                 domain::summary::SummaryRequest {
                     recording: mock_recording,
-                    additional_context: Some(vec![
-                        "FIXME_THIS_IS_A_PLACEHOLDER".to_string(),
-                    ]),
+                    additional_context: Some(vec!["FIXME_THIS_IS_A_PLACEHOLDER".to_string()]),
                 },
                 transcript,
             );
